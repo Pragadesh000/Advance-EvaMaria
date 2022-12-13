6e163872a3b2987e83ceb0966eceec567705941c
Pragadesh Raj <114950806+Pragadesh000@users.noreply.github.com>
2022-12-13 17:56:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Pragadesh000/Advance-EvaMaria/tree/Master /EvaMaria
+  git clone https://github.com/Pragadesh000/Advance-EvaMaria/tree/Pragadesh000-patch-2 /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
